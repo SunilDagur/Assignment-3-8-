d4acfa48324c9a54fdb5a015a6c72e10913b309b
Sunil Dagur <sunildagur566@gmail.com>
2023-09-06 22:24:39 +0530

    Add project files.

---
+++ Assignment 3(8)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class MyClass
+{
+    
+    private void PrivateMethod()
+    {
+        Console.WriteLine("This is the private method.");
+    }
+
+   
+    public void PublicMethod()
+    {
+        Console.WriteLine("This is the public method.");
+        PrivateMethod(); 
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass();
+
+        
+        myObject.PublicMethod();
+
+        
+
+        Console.ReadLine();
+    }
+}
